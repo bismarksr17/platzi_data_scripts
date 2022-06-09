480577ee231d1e17161a916e04558952b4980597
bismarksr17 <bismark.sr.17@gmail.com>
2022-06-09 19:12:19 -0400

    se agrego algunos comentarios extra

--- 09_python-intermedio/01_list_and_dict.py
+++ 09_python-intermedio/01_list_and_dict.py
@@ -1,4 +1,5 @@
 def run():
+    
     # lista y dict normal
     my_list = [1, 'hello', True, 4.5]
     my_dict = {'firstname': 'Facundo', 'lastname': 'Garcia'}
@@ -16,13 +17,17 @@ def run():
         'integer_nums' : [-1,-2,0,1,2],
         'float_nums' : [1.1, 4.5, 6.4]
     }
-   
+
+    # recorrido de un diccionario de listas  
     for key, value in super_dict.items():
         print(key, '-', value)
-   
+    
+    # recorrido de una lista de diccionarios
     for i in super_list:
         print(i)
  
+
+
 # punto de entrada
 if __name__ == '__main__':
     run()
--- 09_python-intermedio/03_dictionsty_comprehensions.py
+++ 09_python-intermedio/03_dictionsty_comprehensions.py
@@ -1,6 +1,8 @@
 
 # CREA UN DICT, COMO LLAVE NUMEROS DE 1 AL 100, Y COMO VALOR EL CUBO DE LA LLAVE
 # Y QUE NO SEAN DIVICIBLES ENTRA 3
+
+# solucion con funcion normal 
 def run():
     dic = {}
     for i in range(1, 101):
@@ -8,9 +10,12 @@ def run():
              dic[i] = i**3
     print(dic)
 
+# solucion con dict comprehensions
 def run_2():
     dic = { i : i**3 for i in range(1, 101) if i % 3 != 0 }
     print(dic)
+
+
 
 if __name__ == '__main__':
     run_2()
