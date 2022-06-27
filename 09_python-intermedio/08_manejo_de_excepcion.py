9e31c5963e633eeba6fe3f5d5561957850b39f3c
bismarksr17 <bismark.sr.17@gmail.com>
2022-06-27 18:58:13 -0400

    Se complero el reto de manejo de excepciones

--- 09_python-intermedio/08_manejo_de_excepcion.py
+++ 09_python-intermedio/08_manejo_de_excepcion.py
@@ -1,9 +1,15 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError('No se puede ingrasar un valor negativo')
+        divisors = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
 
 def run():
     try:
