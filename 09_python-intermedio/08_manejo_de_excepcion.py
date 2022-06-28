b9f5836823d75dabbe499851f993c57e3f48a683
bismarksr17 <bismark.sr.17@gmail.com>
2022-06-27 20:28:14 -0400

    Mejora del reto de manejo de excepciones

--- 09_python-intermedio/08_manejo_de_excepcion.py
+++ 09_python-intermedio/08_manejo_de_excepcion.py
@@ -1,23 +1,22 @@
 def divisors(num):
-    try:
-        if num < 0:
-            raise ValueError('No se puede ingrasar un valor negativo')
-        divisors = []
-        for i in range(1, num + 1):
-            if num % i == 0:
-                divisors.append(i)
-        return divisors
-    except ValueError as ve:
-        print(ve)
-        return False
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors    
 
 def run():
     try:
         num = int(input("Ingresa un número: "))
+        if num < 0:
+            raise TypeError('No se aceptan valores negativos')
         print(divisors(num))
         print("Termino el programa")
     except ValueError:
         print("Debes ingresar un numero")
+    except TypeError as te:
+        print(te)
+        return False
 
 
 if __name__ == "__main__":
