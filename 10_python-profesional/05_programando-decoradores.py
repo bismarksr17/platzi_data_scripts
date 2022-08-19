5ca5e8a27bed6852b774ad66c05c1ae641b09bbc
bismark.sr.17 <bismarksr17@IAG_UTEA.localdomain>
2022-08-19 10:50:41 -0400

    se termino de implementar el segundo ejemplo de decoradores

--- 10_python-profesional/05_programando-decoradores.py
+++ 10_python-profesional/05_programando-decoradores.py
@@ -1,17 +1,29 @@
 from datetime import datetime
 
+# decorador para calcular tiempo de ejecucion de una función
+# se usa *args y **kwargs para validad cualquier numero de parametros
 def execution_time(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         initial_time = datetime.now()
-        func()
+        func(*args, **kwargs)
         final_time = datetime.now()
         time_elapsed = final_time - initial_time
         print('Pasaron ' + str(time_elapsed.total_seconds()) + ' segundos')
     return wrapper
 
+# se decora la funcion
 @execution_time
 def random_func():
     for _ in range(1, 1000000000):
         pass
 
+# se ejecuta la funcion
 random_func()
+
+# se decora otra funcion con parametros
+@execution_time
+def sumar(a, b):
+    return a + b
+
+# se ejecuta la funcion con parametros
+sumar(13, 43)
